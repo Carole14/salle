7fcb00813057bc303342776880ffde9c26ca90f3
Carole Belle <b.carole14@gmail.com>
2022-07-14 17:11:19 +0200

    creation table partenaire and user. Migration of tables. Creation controller login.

---
+++ src/Entity/Partenaire.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PartenaireRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PartenaireRepository::class)]
+class Partenaire
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column()]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $active = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $technical_contact = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $commercial_contact = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getActive(): ?string
+    {
+        return $this->active;
+    }
+
+    public function setActive(string $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTechnicalContact(): ?string
+    {
+        return $this->technical_contact;
+    }
+
+    public function setTechnicalContact(?string $technical_contact): self
+    {
+        $this->technical_contact = $technical_contact;
+
+        return $this;
+    }
+
+    public function getCommercialContact(): ?string
+    {
+        return $this->commercial_contact;
+    }
+
+    public function setCommercialContact(?string $commercial_contact): self
+    {
+        $this->commercial_contact = $commercial_contact;
+
+        return $this;
+    }
+}
